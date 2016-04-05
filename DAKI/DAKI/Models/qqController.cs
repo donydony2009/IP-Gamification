936c1ba5fecbc847e464e6d4199981159e2dbe97
Dony <donydony2009@gmail.com>
2016-04-05 20:18:22 +0300

    Test Merging

---
+++ DAKI/DAKI/Models/qqController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAKI.Models
+{
+    public class qqController : Controller
+    {
+        //
+        // GET: /qq/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
