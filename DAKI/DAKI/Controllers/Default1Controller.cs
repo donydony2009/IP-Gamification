4a4786704837b5e9891af186b7e0c548032b1f28
Dony <donydony2009@gmail.com>
2016-04-12 03:05:48 +0300

    Added the start of the shop

---
+++ DAKI/DAKI/Controllers/Default1Controller.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAKI.Models;
+
+namespace DAKI.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        private UsersContext db = new UsersContext();
+
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View(db.Prizes.ToList());
+        }
+
+        //
+        // GET: /Default1/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Prize prize = db.Prizes.Find(id);
+            if (prize == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prize);
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        public ActionResult Create(Prize prize)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Prizes.Add(prize);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(prize);
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Prize prize = db.Prizes.Find(id);
+            if (prize == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prize);
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Prize prize)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prize).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(prize);
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Prize prize = db.Prizes.Find(id);
+            if (prize == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prize);
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prize prize = db.Prizes.Find(id);
+            db.Prizes.Remove(prize);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
