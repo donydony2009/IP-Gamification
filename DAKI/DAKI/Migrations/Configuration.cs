db5bd9d666ce3f505a62d1a0947e675fae74a266
KarinaNazare <nazare.ana.karina@gmail.com>
2016-05-30 01:12:00 +0300

    Ofofof

--- DAKI/DAKI/Migrations/Configuration.cs
+++ DAKI/DAKI/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace DAKI.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using DAKI.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<DAKI.Models.UsersContext>
     {
@@ -14,18 +15,13 @@ namespace DAKI.Migrations
 
         protected override void Seed(DAKI.Models.UsersContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Departments.AddOrUpdate( x => x.DepartmentId,
+                    new Department() { DepartmentId = 1, Title = "Administration" , Description = "Administration department", ParentId = null, Rules = true },
+                    new Department() { DepartmentId = 2, Title = "Accounting", Description = "Accounting department", ParentId = 1, Rules = true },
+                    new Department() { DepartmentId = 3, Title = "Research", Description = "Research department", ParentId = 1, Rules = true },
+                    new Department() { DepartmentId = 4, Title = "Operations", Description = "Operations department", ParentId = 3, Rules = true },
+                    new Department() { DepartmentId = 5, Title = "Sales", Description = "Sales department", ParentId = 4, Rules = true }
+                );
         }
     }
 }
